4a2efd872ca96cd7c349eb4f8908a914e3a9bef8
以宁 <316167645@qq.com>
2022-06-26 11:44:34 +0800

    路由配置

--- src/views/Video/index.vue
+++ src/views/Video/index.vue
@@ -1,13 +1,10 @@
 <template>
-<div>
-  <!-- 一级占位符 -->
-  <router-view></router-view>
-</div>
+  <div>视频</div>
 </template>
 
 <script>
 export default {
-  created () {},
+  created () { },
   data () {
     return {}
   },
@@ -20,5 +17,4 @@ export default {
 </script>
 
 <style scoped lang='less'>
-
 </style>
